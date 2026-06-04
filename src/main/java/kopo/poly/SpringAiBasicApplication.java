7a094b1a2610a04f4b7f3b6c1a29a07d1155521f
data8320-22 <rlaehddnwkd1107@gmail.com>
2026-06-04 11:47:11 +0900

    실습2도 해결완!

--- src/main/java/kopo/poly/SpringAiBasicApplication.java
+++ src/main/java/kopo/poly/SpringAiBasicApplication.java
@@ -2,10 +2,14 @@ package kopo.poly;
 
 import kopo.poly.dto.NlpDTO;
 import kopo.poly.dto.OcrDTO;
+import kopo.poly.dto.StudentDTO;
 import kopo.poly.service.INlpService;
 import kopo.poly.service.IOcrService;
+import kopo.poly.service.IStudentService;
+import kopo.poly.service.impl.StudentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,89 +18,146 @@ import java.util.*;
 
 @Slf4j
 @RequiredArgsConstructor
+//@MapperScan("kopo.poly.mapper")
 @SpringBootApplication
 public class SpringAiBasicApplication implements CommandLineRunner {
 
 
-    private final IOcrService ocrService;
+//    private final IOcrService ocrService;
+//
+//    private final INlpService nlpService;
 
-    private final INlpService nlpService;
+    private final IStudentService studentService;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAiBasicApplication.class, args);
 
     }
-//비공개로 커밋되서 다시 커밋할려고 추가하는 주석 ㅎㅎㅎㅎㅎ
+
 
     @Override
     public void run(String... args) throws Exception {
 
         log.info("자바 프로그래밍 시작!!");
 
-        String filePath = "image";
-        String fileName = "sample01.jpg";
-        OcrDTO pDTO = new OcrDTO();
+        StudentDTO pDTO;
+        List<StudentDTO> rList;
 
-        pDTO.setFilePath(filePath);
-        pDTO.setFileName(fileName);
+        pDTO = new StudentDTO();
 
-        OcrDTO rDTO = ocrService.getReadforImageText(pDTO);
-
-        String result = rDTO.getResult();
-
-        log.info("인식된 문자열");
-        log.info(result);
+        pDTO.setUserId("hglee67");
+        pDTO.setUserName("이협건");
+        pDTO.setEmail("hglee67@kopo.ac.kr");
+        pDTO.setAddr("서울");
 
 
+        rList = studentService.insertStudent(pDTO);
 
-        log.info("자바 프로그래밍 종료!!");
+        rList.forEach(dto -> {
+            log.info("DB에 저장된 아이디: " + dto.getUserId());
+            log.info("DB에 저장된 이름: " + dto.getUserName());
+            log.info("DB에 저장된 이메일: " + dto.getEmail());
+            log.info("DB에 저장된 주소: " + dto.getAddr());
+        });
+        //수정하기
 
+        pDTO = new StudentDTO();
 
-        NlpDTO nlpDTO = nlpService.getNouns(result);
+        pDTO.setUserId("hglee67");
+        pDTO.setUserName("이협건_수정");
+        pDTO.setEmail("hglee67@kopo.ac.kr_수정");
+        pDTO.setAddr("서울_수정");
 
+        rList = studentService.updateStudent(pDTO);
 
-        List<String> nouns = nlpDTO.getNouns();
+        rList.forEach(dto -> {
+            log.info("DB에 저장된 아이디 : " + dto.getUserId());
+            log.info("DB에 저장된 이름: " + dto.getUserName());
+            log.info("DB에 저장된 이메일: " + dto.getEmail());
+            log.info("DB에 저장된 주소 : " + dto.getAddr());
+        });
 
-        Set<String> distinct = new HashSet<>(nouns);
+        pDTO = new StudentDTO();
 
-        Map<String, Integer> rMap = new HashMap<>();
+        pDTO.setUserId("hglee67");
 
-        for (String s : distinct) {
-            int count = Collections.frequency(nouns, s);
-            rMap.put(s, count);
+        rList = studentService.deleteStudent(pDTO);
 
-            log.info(s, count);
-        }
-
-
-
-
-        NlpDTO plainDTO = nlpService.getPlainText(result);
-
-        log.info("형태소별 품사 분석 결과 : " + plainDTO.getResult());
-
-        rMap = new HashMap<>();
-
-
-        log.info("중복 제거 수행 전 단어 수 : " + nouns.size());
-        log.info("중복 제거 수행 후 단어 수 : " + distinct.size());
-
-
-        for (String s : distinct) {
-            int count = Collections.frequency(nouns, s);
-            rMap.put(s, count);
-
-            log.info(s + " : " + count);
-        }
-        List<Map.Entry<String, Integer>> sortResult = new LinkedList<>(rMap.entrySet());
-        Collections.sort(sortResult, (o1, o2) -> o2.getValue().compareTo(o1.getValue()));
-
-        log.info("가장 많이 사용된 단어는? : " + sortResult);
+        rList.forEach(dto -> {
+            log.info("DB에 저장된 아이디 : " + dto.getUserId());
+            log.info("DB에 저장된 이름: " + dto.getUserName());
+            log.info("DB에 저장된 이메일: " + dto.getEmail());
+            log.info("DB에 저장된 주소 : " + dto.getAddr());
+        });
 
         log.info("자바 프로그래밍 종료!!");
 
+        }
 
-    }
+//        String filePath = "image";
+//        String fileName = "sample01.jpg";
+//        OcrDTO pDTO = new OcrDTO();
+//
+//        pDTO.setFilePath(filePath);
+//        pDTO.setFileName(fileName);
+//
+//        OcrDTO rDTO = ocrService.getReadforImageText(pDTO);
+//
+//        String result = rDTO.getResult();
+//
+//        log.info("인식된 문자열");
+//        log.info(result);
+//
+//
+//
+//        log.info("자바 프로그래밍 종료!!");
+//
+//
+//        NlpDTO nlpDTO = nlpService.getNouns(result);
+//
+//
+//        List<String> nouns = nlpDTO.getNouns();
+//
+//        Set<String> distinct = new HashSet<>(nouns);
+//
+//        Map<String, Integer> rMap = new HashMap<>();
+//
+//        for (String s : distinct) {
+//            int count = Collections.frequency(nouns, s);
+//            rMap.put(s, count);
+//
+//            log.info(s, count);
+//        }
+//
+//
+//
+//
+//        NlpDTO plainDTO = nlpService.getPlainText(result);
+//
+//        log.info("형태소별 품사 분석 결과 : " + plainDTO.getResult());
+//
+//        rMap = new HashMap<>();
+//
+//
+//        log.info("중복 제거 수행 전 단어 수 : " + nouns.size());
+//        log.info("중복 제거 수행 후 단어 수 : " + distinct.size());
+//
+//
+//        for (String s : distinct) {
+//            int count = Collections.frequency(nouns, s);
+//            rMap.put(s, count);
+//
+//            log.info(s + " : " + count);
+//        }
+//        List<Map.Entry<String, Integer>> sortResult = new LinkedList<>(rMap.entrySet());
+//        Collections.sort(sortResult, (o1, o2) -> o2.getValue().compareTo(o1.getValue()));
+//
+//        log.info("가장 많이 사용된 단어는? : " + sortResult);
+//
+//        log.info("자바 프로그래밍 종료!!");
+//
+
+
 
 
 }
--- src/main/java/kopo/poly/service/impl/StudentService.java
+++ src/main/java/kopo/poly/service/impl/StudentService.java
@@ -1,4 +1,99 @@
 package kopo.poly.service.impl;
 
-public class StudentService {
-}
+import kopo.poly.dto.StudentDTO;
+import kopo.poly.mapper.IStudentMapper;
+import kopo.poly.service.IStudentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class StudentService implements IStudentService {
+
+    private final IStudentMapper studentMapper;
+
+    @Override
+    public List<StudentDTO> insertStudent(StudentDTO pDTO) throws Exception{
+
+        String className = this.getClass().getName();
+        log.info("{}.insertStudent start!", className);
+
+        Optional<StudentDTO> res = Optional.ofNullable(
+                studentMapper.getStudent(pDTO)
+        );
+        if (res.isEmpty()) {
+            studentMapper.insertStudent(pDTO);
+            log.info("학생 등록 완료 - ID: {}", pDTO.getUserId());
+        } else {
+            log.warn("학생 등록 실패 - 이미 존재하는 ID: {}", pDTO.getUserId());
+        }
+
+        List<StudentDTO> rList = Optional.ofNullable(
+                studentMapper.getStudentList()
+        ).orElseGet(ArrayList::new);
+
+        log.info("{}.insertStudent End!", className);
+
+        return rList;
+
+        }
+    public List<StudentDTO> updateStudent(StudentDTO pDTO) throws Exception{
+
+        String className = this.getClass().getName();
+        log.info("{}.updateStudent start!", className);
+
+        Optional<StudentDTO> res = Optional.ofNullable(studentMapper.getStudent(pDTO));
+
+        if (res.isPresent()) {
+
+            studentMapper.updateStudent(pDTO);
+
+            log.info("{}님이 수정되었습니다.", pDTO.getUserId());
+        } else{
+            log.warn("수정 실패 - 존재하지 않는 회원입니다 : {}", pDTO.getUserId());
+        }
+
+        List<StudentDTO> rList =  Optional.ofNullable(
+                studentMapper.getStudentList()
+        ).orElseGet(ArrayList::new);
+
+        log.info("{}.updateStudent End!", className);
+
+        return rList;
+        }
+
+    @Override
+    public List<StudentDTO> deleteStudent(StudentDTO pDTO) throws Exception{
+
+        String className = this.getClass().getName();
+        log.info("{}.deleteStudent start!", className);
+
+        Optional<StudentDTO> res = Optional.ofNullable(
+                studentMapper.getStudent(pDTO));
+
+        if (res.isPresent()) {
+            studentMapper.deleteStudent(pDTO);
+
+            log.info("{}님이 삭제되었습니다.", pDTO.getUserId());
+        } else {
+            log.warn("삭제 실패 - 존재하지 않는 회원입니다: {}", pDTO.getUserId());
+        }
+
+        List<StudentDTO> rList = Optional.ofNullable(
+                studentMapper.getStudentList()
+        ).orElseGet(ArrayList::new);
+
+        log.info("{}.deleteStudent End!", className);
+
+        return rList;
+        }
+
+    }
+
+
